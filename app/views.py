1b719864de2bea605ef72fcc7ce27be4750b6e80
Karan Verma <rockinkaran@yahoo.com>
2023-01-28 23:43:28 +0530

    logic and js

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
+    timeframe = request.GET.get('timeframe')
+    print(timeframe)
+    nodes = request.GET.getlist('nodes', '')
+    print(nodes)
+    url = "https://rs01sv01.bnl.cos.lan:9002/dpa-api/report/"
+    for node in nodes:
+        if node:
+            response = requests.get("https://reqres.in/api/users/" + node)
+            print(response.json())
     return render(request, "home.html")
