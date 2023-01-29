764f4bae943d81e0cee8d180822f18a44d5dd285
Karan Verma <rockinkaran@yahoo.com>
2023-01-29 22:49:38 +0530

    xml api

--- app/views.py
+++ app/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import xmltodict
 
 # Create your views here.
 def index(request):
     timeframe = request.GET.get('timeframe')
     print(timeframe)
+    file_format = request.GET.get('format')
+    print(file_format)
+    template = request.GET.get('template')
+    print(template)
     nodes = request.GET.getlist('nodes', '')
     print(nodes)
-    url = "https://rs01sv01.bnl.cos.lan:9002/dpa-api/report/"
+    url = "https://rs01sv01.bnl.cos.lan:9002/dpa-api/report/" # <16 digit ID>
     for node in nodes:
         if node:
-            response = requests.get("https://reqres.in/api/users/" + node)
-            print(response.json())
+            response = requests.get("https://api.shipwire.com/exec/FulfillmentServices.php")
+            dict_data = xmltodict.parse(response.content)
+            print(dict_data)
+            # print(f"ID for node {node} : {dict_data['licenses']['license']['id']}")
     return render(request, "home.html")
