c82f97299782d5b30357831f929d6f60f9610bd5
Karan Verma <rockinkaran@yahoo.com>
2023-02-04 11:55:05 +0530

    session.get

--- app/views.py
+++ app/views.py
@@ -44,7 +44,7 @@ def index(request):
             xml.replace("<template>", template)
             xml.replace("<node_id>", node_id)
             xml.replace("<format_type>", file_format)
-            r = requests.post(url + node_id, data=xml, headers=headers, verify=False)
+            r = session.post(url + node_id, data=xml, headers=headers, verify=False)
             print(r)
             print(r.content)
             pdfs.append(r.content)
